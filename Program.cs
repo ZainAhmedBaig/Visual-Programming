e37ad014f96d52b7ff97466f96b9d30e1c2da03d
ZainAhmedBaig <56203338+ZainAhmedBaig@users.noreply.github.com>
2019-10-14 04:22:42 -0700

    Print CV using simple Write commands

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" ");
+            Console.WriteLine("------------CV-----------");
+            Console.WriteLine("Name  : Zain Ahmed Baig");
+            Console.WriteLine("CGPA  : 3.14");
+            Console.WriteLine("Age   : 22");
+            Console.WriteLine("Depart: BSCS");
+            Console.WriteLine("-------------------------");
+            Console.ReadLine();
+        }
+    }
+}
