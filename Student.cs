520ba5ad5a06da0ee4f307df5240772cb2c073b6
ZainAhmedBaig <56203338+ZainAhmedBaig@users.noreply.github.com>
2019-12-23 19:15:03 +0500

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public float cgpa { get; set; }
+        public string gender { get; set; }
+
+        public override String ToString()
+        {
+            return name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!Convert.IsDBNull(obj))
+            {
+                Student s = (Student)obj;
+                return this.name.Equals(s.name);
+            }
+
+            return false;
+        }
+
+    }
+}
